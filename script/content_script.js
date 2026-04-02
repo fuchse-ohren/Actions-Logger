a08bf564bd002f6aea9046688c9a53d47ab84d5b
fuchse-ohren <git@kitsune.aleeas.com>
2026-04-02 15:10:08 +0900

    デザインの変更

--- script/content_script.js
+++ script/content_script.js
@@ -1,87 +1,105 @@
 //設定を取得する関数
-function _get_setting(type){
-	return new Promise(function (resolve) {
-		chrome.runtime.sendMessage({"type":type}, function(res) {
-			resolve(res);
-		});
-	});
+function _get_setting(type) {
+  return new Promise(function (resolve) {
+    chrome.runtime.sendMessage({ type: type }, function (res) {
+      resolve(res);
+    });
+  });
 }
 
-function _putlog(url,log){
-	return new Promise(function (resolve) {
-		chrome.runtime.sendMessage({"type":"putlog","message":log,"url":url}, function(res) {
-			resolve(res);
-		});
-	});
+function _putlog(url, log) {
+  return new Promise(function (resolve) {
+    chrome.runtime.sendMessage(
+      { type: "putlog", message: log, url: url },
+      function (res) {
+        resolve(res);
+      },
+    );
+  });
 }
 
-async function poll(){
-	const targetURL = await _get_setting('targetURL');
-	const exclusionURL = await _get_setting('exclusionURL');
-	const url = location.href.split(/(#|\?|\/$|\/\?)/)[0];
-	
-	//ターゲットURLのみ記録する
-	if(targetURL.test(url) && !exclusionURL.test(url)){
-		var log = {};
-		log[url] = {"button":[],"link":[],"input":[]};
-		
-		//ページ内のアクション一覧を取得
-		var but = document.getElementsByTagName("button")
-		var but_list = new Set(log[url]["button"])
-		for(i=0;i<but.length;i++){
-		  if(but[i].innerText.trim() != ""){
-			but_list.add(but[i].innerText.trim())
-		  }
-		}
+async function poll() {
+  const targetURL = await _get_setting("targetURL");
+  const exclusionURL = await _get_setting("exclusionURL");
+  const url = location.href.split(/(#|\?|\/$|\/\?)/)[0];
 
-		var a = document.getElementsByTagName("a")
-		var a_list = new Set(log[url]["link"])
-		for(i=0;i<a.length;i++){
-		  if(a[i].innerText.trim() != ""){
-			a_list.add(a[i].innerText.trim())
-		  }else if(a[i].href.trim() != "" && a[i].href != "#" && a[i].href != "javascript:void(0)"){
-			a_list.add(a[i].href.trim())
-		  }
-		};
+  //ターゲットURLのみ記録する
+  if (targetURL.test(url) && !exclusionURL.test(url)) {
+    var log = {};
+    log[url] = { button: [], link: [], input: [] };
 
-		var input = document.getElementsByTagName("input")
-		var input_list = new Set(log[url]["input"]);
-		for(i=0;i<input.length;i++){
-		  if((input[i].type == "submit" || input[i].type == "reset") && input[i].value.trim() != ""){
-		    input_list.add(input[i].value.trim())
-		  }else if(input[i].type == "image" && input[i].alt.trim() != ""){
-		    input_list.add(input[i].alt.trim())
-		  }else if(input[i].placeholder.trim() != "" && input[i].type != "hidden"){
-			input_list.add(input[i].placeholder.trim())
-		  }else if(input[i].name.trim() != "" && input[i].type != "hidden"){
-			input_list.add(input[i].name.trim())
-		  }
-		}
+    //ページ内のアクション一覧を取得
 
-		log[url]["button"] =  Array.from(but_list);
-		log[url]["link"] =  Array.from(a_list);
-		log[url]["input"] =  Array.from(input_list);
-		
-		res = await _putlog(url,log);
-		console.log(res);
-	}
+    // ボタン
+    var but = document.getElementsByTagName("button");
+    var but_list = new Set(log[url]["button"]);
+    for (i = 0; i < but.length; i++) {
+      if (but[i].innerText.trim() != "") {
+        but_list.add(but[i].innerText.trim());
+      }
+    }
+
+    // リンク
+    var a = document.getElementsByTagName("a");
+    var a_list = new Set(log[url]["link"]);
+    for (i = 0; i < a.length; i++) {
+      if (a[i].innerText.trim() != "") {
+        a_list.add(a[i].innerText.trim());
+      } else if (
+        a[i].href.trim() != "" &&
+        a[i].href != "#" &&
+        a[i].href != "javascript:void(0)"
+      ) {
+        a_list.add(a[i].href.trim());
+      }
+    }
+
+    // 入力
+    var input = document.getElementsByTagName("input");
+    var input_list = new Set(log[url]["input"]);
+    for (i = 0; i < input.length; i++) {
+      if (
+        // submit,resetボタンのvalueがでない場合
+        (input[i].type == "submit" || input[i].type == "reset") &&
+        input[i].value.trim() != ""
+      ) {
+        input_list.add(input[i].value.trim());
+      } else if (input[i].type == "image" && input[i].alt.trim() != "") {
+        input_list.add(input[i].alt.trim());
+      } else if (
+        input[i].placeholder.trim() != "" &&
+        input[i].type != "hidden"
+      ) {
+        input_list.add(input[i].placeholder.trim());
+      } else if (input[i].name.trim() != "" && input[i].type != "hidden") {
+        input_list.add(input[i].name.trim());
+      }
+    }
+
+    log[url]["button"] = Array.from(but_list);
+    log[url]["link"] = Array.from(a_list);
+    log[url]["input"] = Array.from(input_list);
+
+    res = await _putlog(url, log);
+    console.log(res);
+  }
 }
 
-async function main(){
-	const spamode = await _get_setting('SPA');
-	console.log(spamode);
-	switch(spamode){
-		case "0":
-			poll();
-			break;
-		case "1":
-			const observer = new MutationObserver(poll);
-			observer.observe(document, { childList: true, subtree: true });
-			break;
-		case "2":
-			setInterval(poll,5000);
-			break;
-	}
+async function main() {
+  const spamode = await _get_setting("SPA");
+  console.log(spamode);
+  switch (spamode) {
+    case "0":
+      poll();
+      break;
+    case "1":
+      const observer = new MutationObserver(poll);
+      observer.observe(document, { childList: true, subtree: true });
+      break;
+    case "2":
+      setInterval(poll, 5000);
+      break;
+  }
 }
 
 main();
--- settings/script.js
+++ settings/script.js
@@ -1,89 +1,119 @@
-function sv(){
-	var message = document.getElementById('message');
-
-	if(document.getElementById("regex").value === ""){
-		message.innerText = "何か入力したらどうですか？";
-		message.style.color="#e78B90";
-		return;
-	}
-	
-	//対象URLの登録
-	try{
-		var thisRegex = new RegExp(document.getElementById("regex").value);
-		localStorage.setItem("targetURL", document.getElementById("regex").value);
-		console.log(localStorage.getItem("targetURL"));
-		message.innerText = "登録しました";
-		message.style.color="#e78B90";
-	}catch{
-		message.innerText = "対象URLの正規表現が正しくありません";
-		message.style.color="#e78B90";
-	}
-	
-	//除外URLの登録
-		if(document.getElementById("excregex").value === ""){
-			message.innerText = "何か入力したらどうですか？";
-			message.style.color="#e78B90";
-			return;
-	}
-	
-	try{
-		var thisRegex = new RegExp(document.getElementById("excregex").value);
-		localStorage.setItem("exclusionURL", document.getElementById("excregex").value);
-		console.log(localStorage.getItem("exclusionURL"));
-	}catch{
-		message.innerText = "除外URLの正規表現が正しくありません";
-		message.style.color="#e78B90";
-	}
-	
-	//SPAモードの登録
-	try{
-		localStorage.setItem("SPA", document.getElementById("spamode").value);
-		console.log(localStorage.getItem("SPA"));
-		message.innerText = "登録しました";
-		message.style.color="#e78B90";
-	}catch{
-		message.innerText = "SPAモードの設定が登録できませんでした";
-		message.style.color="#e78B90";
-	}
+function clear() {
+  //すべてのメッセージを消す
+  document.getElementById("regex").ariaInvalid = false;
+  document.getElementById("excregex").ariaInvalid = false;
+  document.getElementById("regex-error-text").style.display = "none";
+  document.getElementById("excregex-error-text").style.display = "none";
+  document.getElementById("message").innerText = "";
 }
 
-function rm(){
-	const message = document.getElementById("log_message");
-	localStorage.setItem("log","{}");
-	message.innerText = "ログを消去しました";
-	message.style.color="#e78B90";
-	document.getElementById("logsize").innerText = String(localStorage.getItem("log").length)+" バイト";
+function sv() {
+  //すべてのメッセージを消す
+  clear();
+
+  //対象URLの登録
+  if (document.getElementById("regex").value === "") {
+    document.getElementById("regex-error-text").innerText =
+      "＊この項目は入力必須です";
+    document.getElementById("regex-error-text").style.display = "";
+    document.getElementById("regex").ariaInvalid = true;
+    return;
+  }
+  try {
+    var thisRegex = new RegExp(document.getElementById("regex").value);
+    localStorage.setItem("targetURL", document.getElementById("regex").value);
+    console.log(localStorage.getItem("targetURL"));
+  } catch {
+    document.getElementById("regex-error-text").innerText =
+      "＊正規表現の書式が誤っています";
+    document.getElementById("regex-error-text").style.display = "";
+    document.getElementById("regex").ariaInvalid = true;
+    return;
+  }
+
+  //除外URLの登録
+  if (document.getElementById("excregex").value === "") {
+    document.getElementById("excregex").value = "^$";
+  }
+  try {
+    var thisRegex = new RegExp(document.getElementById("excregex").value);
+    localStorage.setItem(
+      "exclusionURL",
+      document.getElementById("excregex").value,
+    );
+    console.log(localStorage.getItem("exclusionURL"));
+  } catch {
+    document.getElementById("excregex-error-text").innerText =
+      "＊正規表現の書式が誤っています";
+    document.getElementById("excregex").ariaInvalid = true;
+    document.getElementById("excregex-error-text").style.display = "";
+    return;
+  }
+
+  // SPAモードの登録
+  localStorage.setItem("SPA", document.getElementById("spamode").value);
+  console.log(localStorage.getItem("SPA"));
+
+  document.getElementById("message").innerText = "登録しました";
 }
 
-function dl(){
-	const log = localStorage.getItem("log");
-	const blob = new Blob([log], { type: 'application/json' });
-	let a = document.createElement('a');
-	document.body.appendChild(a);
-	a.href = window.URL.createObjectURL(blob);
-	a.download = 'params_log.json';
-	a.click();
-	document.body.removeChild(a);
+function rm() {
+  //すべてのメッセージを消す
+  clear();
+
+  const message = document.getElementById("log_message");
+  localStorage.setItem("log", "{}");
+  message.innerText = "ログを消去しました";
+  document.getElementById("logsize").innerText =
+    String(localStorage.getItem("log").length) + " バイト";
 }
 
-function reset(){
-	localStorage.setItem("targetURL", 'htt(p|ps)://example.net($|/.*)');
-	localStorage.setItem("exclusionURL", '(.*\\.)(jpg|jpeg|png|gif|svg|ico|css|js|woff|woff2|mp4|mp3|ts|m3u8|pdf|doc|docx|ppt|pptx|xls|xlsx|md)$');
-	localStorage.setItem("SPA", 0);
-	message.innerText = "設定をリセットしました";
-	message.style.color="#e78B90";
-	document.getElementById("regex").value = localStorage.getItem("targetURL");
-	document.getElementById("excregex").value = localStorage.getItem("exclusionURL");
-	document.getElementById("logsize").innerText = String(localStorage.getItem("log").length)+" バイト";
-    document.getElementById("spamode").value = localStorage.getItem("SPA");
+function dl() {
+  //すべてのメッセージを消す
+  clear();
+
+  const log = localStorage.getItem("log");
+  const blob = new Blob([log], { type: "application/json" });
+  let a = document.createElement("a");
+  document.body.appendChild(a);
+  a.href = window.URL.createObjectURL(blob);
+  a.download = "params_log.json";
+  a.click();
+  document.body.removeChild(a);
 }
 
-document.getElementById('save').addEventListener('click',sv);
-document.getElementById('logrm').addEventListener('click',rm);
-document.getElementById('logdl').addEventListener('click',dl);
-document.getElementById('reset').addEventListener('click',reset);
+function reset() {
+  //すべてのメッセージを消す
+  clear();
+
+  localStorage.setItem("targetURL", "htt(p|ps)://example.net($|/.*)");
+  localStorage.setItem(
+    "exclusionURL",
+    "(.*\\.)(jpg|jpeg|png|gif|svg|ico|css|js|woff|woff2|mp4|mp3|ts|m3u8|pdf|doc|docx|ppt|pptx|xls|xlsx|md)$",
+  );
+  localStorage.setItem("SPA", "0");
+  message.innerText = "設定をリセットしました";
+  document.getElementById("regex").value = localStorage.getItem("targetURL");
+  document.getElementById("excregex").value =
+    localStorage.getItem("exclusionURL");
+  document.getElementById("spamode").value = localStorage.getItem("SPA");
+  document.getElementById("logsize").innerText =
+    String(localStorage.getItem("log").length) + " バイト";
+}
+
+// 画面開設時処理
+document.getElementById("save").addEventListener("click", sv);
+document.getElementById("logrm").addEventListener("click", rm);
+document.getElementById("logdl").addEventListener("click", dl);
+document.getElementById("reset").addEventListener("click", reset);
+document.getElementById("apply").addEventListener("click", () => {
+  browser.runtime.reload();
+});
 document.getElementById("regex").value = localStorage.getItem("targetURL");
-document.getElementById("excregex").value = localStorage.getItem("exclusionURL");
-document.getElementById("spamode").value = localStorage.getItem("SPA");
-document.getElementById("logsize").innerText = String(localStorage.getItem("log").length)+" バイト";
-document.getElementById('logview').addEventListener('click',()=>{window.open('/view/index.html')});
+document.getElementById("excregex").value =
+  localStorage.getItem("exclusionURL");
+document.getElementById("logsize").innerText =
+  String(localStorage.getItem("log").length) + " バイト";
+document.getElementById("logview").addEventListener("click", () => {
+  window.open("/view/index.html");
+});
